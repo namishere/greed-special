aef1440f457052aaca9c0a8bf9a4764d020c4010
kittenchilly <mario.pikmin.zelda@gmail.com>
2022-09-21 09:40:25 -0500

    Update shared.lua

--- scripts/enums/shared.lua
+++ scripts/enums/shared.lua
@@ -4,28 +4,16 @@ mod.enum = {}
 
 mod.enum.CAIN_ARCADE = RoomType.ROOM_ARCADE + 100
 
-mod.DoorVariant = {
-	BOMB = 0,
-	KEY = 1, -- subtype 1 is maus door and strange door??? may be general use
-	KEY2 = 2,
-	BOMB2 = 3, -- also used by knife door?
-	BARRED = 4,
-	LOCKED = 5, -- fallback behavior for invalid variants
-	UNKNOWN = 6, -- shows bars fading when entering room?
-	CLOSED = 7,
-	OPENED = 8
-}
-
 mod.SpecialRoom = {
-	[RoomType.ROOM_ARCADE] = {variant = mod.DoorVariant.KEY, string = "arcade"},
-	[RoomType.ROOM_CHALLENGE] = {variant = mod.DoorVariant.OPENED, string = "challenge"},
-	[RoomType.ROOM_LIBRARY] = {variant = mod.DoorVariant.KEY, string = "library"},
-	[RoomType.ROOM_SACRIFICE] = {variant = mod.DoorVariant.OPENED, string = "sacrifice"},
-	[RoomType.ROOM_ISAACS] = {variant = mod.DoorVariant.BOMB2, string = "isaacs"},
-	[RoomType.ROOM_BARREN] = {variant = mod.DoorVariant.BOMB2, string = "barren"},
-	[RoomType.ROOM_CHEST] = {variant = mod.DoorVariant.KEY2, string = "chest"},
-	[RoomType.ROOM_DICE] = {variant = mod.DoorVariant.KEY2, string = "dice"},
-	[RoomType.ROOM_PLANETARIUM] = {variant = mod.DoorVariant.KEY, string = "planetarium"},
+	[RoomType.ROOM_ARCADE] = {variant = DoorVariant.DOOR_LOCKED, string = "arcade"},
+	[RoomType.ROOM_CHALLENGE] = {variant = DoorVariant.DOOR_UNLOCKED, string = "challenge"},
+	[RoomType.ROOM_LIBRARY] = {variant = DoorVariant.DOOR_LOCKED, string = "library"},
+	[RoomType.ROOM_SACRIFICE] = {variant = DoorVariant.DOOR_UNLOCKED, string = "sacrifice"},
+	[RoomType.ROOM_ISAACS] = {variant = DoorVariant.DOOR_LOCKED_CRACKED, string = "isaacs"},
+	[RoomType.ROOM_BARREN] = {variant = DoorVariant.DOOR_LOCKED_CRACKED, string = "barren"},
+	[RoomType.ROOM_CHEST] = {variant = DoorVariant.DOOR_LOCKED_DOUBLE, string = "chest"},
+	[RoomType.ROOM_DICE] = {variant = DoorVariant.DOOR_LOCKED_DOUBLE, string = "dice"},
+	[RoomType.ROOM_PLANETARIUM] = {variant = DoorVariant.DOOR_LOCKED, string = "planetarium"},
 
-	[mod.enum.CAIN_ARCADE] = {variant = mod.DoorVariant.KEY, string = "arcade"},
+	[mod.enum.CAIN_ARCADE] = {variant = DoorVariant.DOOR_LOCKED, string = "arcade"},
 }
